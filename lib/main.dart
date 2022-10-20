2cf69d1462bfe699f84545cdb8a3f84fe15c5db4
Ahlam Albalawi <91871608+AHLAM198@users.noreply.github.com>
2022-10-20 12:20:24 +0300

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,304 @@
+import 'dart:async';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:sudoku_ahlam_lamis/sudokuWidget.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_animated_dialog/flutter_animated_dialog.dart';//check if f or t
+import 'package:sudoku_solver_generator/sudoku_solver_generator.dart';
+import 'alerts/all.dart';
+import 'board_style.dart';
+import 'styles.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  // SystemChrome.setPreferredOrientations(
+  //     [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+  // SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: []);
   runApp(const MyApp());
+}
+String x="";
+ void Wronggame  (){
+  x="Wrong game concept";
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
+  static const String versionNumber = '2.4.1';
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Sudoku app',
-      home: const Sudokuwidget(),
+      title: 'Sudoku',
       debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => HomePageState();
+}
+
+class HomePageState extends State<HomePage> {
+  bool firstRun = true;
+  bool gameOver = false;
+  int timesCalled = 0;
+  bool isButtonDisabled = false;
+
+  late List<List<List<int>>> gameList;
+  late List<List<int>> game;
+  late List<List<int>> gameCopy;
+  late List<List<int>> gameSolved;
+  static String platform = () {
+    return defaultTargetPlatform
+        .toString()
+        .replaceFirst("TargetPlatform.", "")
+        .toLowerCase();
+  }();
+
+  @override
+  void initState() {
+    newGame('easy');
+    super.initState();
+  }
+
+  void checkResult() {
+    try {
+      if (SudokuUtilities.isSolved(game)) {
+        isButtonDisabled = !isButtonDisabled;
+        gameOver = true;
+        Timer(const Duration(milliseconds: 500), () {
+          showAnimatedDialog<void>(
+              animationType: DialogTransitionType.fadeScale,
+              barrierDismissible: true,
+              duration: const Duration(milliseconds: 350),
+              context: context,
+              builder: (_) => const AlertGameOver()).whenComplete(() {
+            if (AlertGameOver.newGame) {
+              newGame();
+              AlertGameOver.newGame = false;
+            } else if (AlertGameOver.restartGame) {
+              restartGame();
+              AlertGameOver.restartGame = false;
+            }
+          });
+        });
+      }
+    } on InvalidSudokuConfigurationException {
+       return Wronggame() ;
+    }
+  }
+
+  static Future<List<List<List<int>>>> getNewGame(
+      [String difficulty = 'easy']) async {
+    int emptySquares;
+    switch (difficulty) {
+      case 'test':
+        {
+          emptySquares = 2;
+        }
+        break;
+
+      case 'easy':
+        {
+          emptySquares = 27;
+        }
+        break;
+
+      default:
+        {
+          emptySquares = 2;
+        }
+        break;
+    }
+
+    SudokuGenerator generator = SudokuGenerator(emptySquares: emptySquares);
+    return [generator.newSudoku, generator.newSudokuSolved];
+  }
+
+  static List<List<int>> copyGrid(List<List<int>> grid) {
+    return grid.map((row) => [...row]).toList();
+  }
+
+  void setGame(int mode, [String difficulty = 'easy']) async {
+    if (mode == 1) {
+      game = List.filled(9, [0, 0, 0, 0, 0, 0, 0, 0, 0]);
+      gameCopy = List.filled(9, [0, 0, 0, 0, 0, 0, 0, 0, 0]);
+      gameSolved = List.filled(9, [0, 0, 0, 0, 0, 0, 0, 0, 0]);
+    } else {
+      gameList = await getNewGame(difficulty);
+      game = gameList[0];
+      gameCopy = copyGrid(game);
+      gameSolved = gameList[1];
+    }
+  }
+
+  void showSolution() {
+    setState(() {
+      game = copyGrid(gameSolved);
+      isButtonDisabled =
+          !isButtonDisabled ? !isButtonDisabled : isButtonDisabled;
+      gameOver = true;
+    });
+  }
+
+  void newGame([String difficulty = 'easy']) {
+    Future.delayed(const Duration(milliseconds: 200), () {
+      setState(() {
+        setGame(2, difficulty);
+        isButtonDisabled =
+            isButtonDisabled ? !isButtonDisabled : isButtonDisabled;
+        gameOver = false;
+      });
+    });
+  }
+
+  void restartGame() {
+    setState(() {
+      game = copyGrid(gameCopy);
+      isButtonDisabled =
+          isButtonDisabled ? !isButtonDisabled : isButtonDisabled;
+      gameOver = false;
+    });
+  }
+
+  List<SizedBox> createButtons() {
+    if (firstRun) {
+      setGame(1);
+      firstRun = false;
+    }
+
+    List<SizedBox> buttonList = List<SizedBox>.filled(9, const SizedBox());
+    for (var i = 0; i <= 8; i++) {
+      var k = timesCalled;
+      
+      
+      buttonList[i] = SizedBox(
+        key: Key('grid-button-$k-$i'),
+        width: buttonSize(),
+        height: buttonSize(),
+        child: TextButton(
+          onPressed: isButtonDisabled || gameCopy[k][i] != 0
+              ? null
+              : () {
+                  showAnimatedDialog<void>(
+                          animationType: DialogTransitionType.fade,
+                          barrierDismissible: true,
+                          duration: const Duration(milliseconds: 300),
+                          context: context,
+                          builder: (_) => const AlertNumbersState())
+                      .whenComplete(() {
+                    callback([k, i], AlertNumbersState.number);
+                    AlertNumbersState.number = null;
+                  });
+                },
+          onLongPress: isButtonDisabled || gameCopy[k][i] != 0
+              ? null
+              : () => callback([k, i], 0),
+          style: ButtonStyle(
+            backgroundColor:
+                MaterialStateProperty.all<Color>(buttonColor(k, i)),
+            foregroundColor: MaterialStateProperty.resolveWith<Color>(
+                (Set<MaterialState> states) {
+              if (states.contains(MaterialState.disabled)) {
+                return gameCopy[k][i] == 0
+                    ? emptyColor(gameOver)
+                    : Colors.black;
+              }
+
+              return game[k][i] == 0
+                  ? buttonColor(k, i)
+                  : Styles.primaryColor;
+            }),
+            shape: MaterialStateProperty.all<OutlinedBorder>(
+                RoundedRectangleBorder(
+              borderRadius: buttonEdgeRadius(k, i),
+            )),
+            side: MaterialStateProperty.all<BorderSide>(BorderSide(
+              width: 1,
+              style: BorderStyle.solid,
+            )),
+          ),
+          child: Text(
+            game[k][i] != 0 ? game[k][i].toString() : ' ',
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: buttonFontSize()),
+          ),
+        ),
+      );
+    }
+    timesCalled++;
+    if (timesCalled == 9) {
+      timesCalled = 0;
+    }
+    return buttonList;
+  }
+
+  Row oneRow() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: createButtons(),
+    );
+  }
+
+  List<Row> createRows() {
+    List<Row> rowList = List<Row>.generate(9, (i) => oneRow());
+    return rowList;
+  }
+
+  void callback(List<int> index, int? number) {
+    setState(() {
+      if (number == null) {
+        return;
+      } else if (number == 0) {
+        game[index[0]][index[1]] = number;
+      } else {
+        game[index[0]][index[1]] = number;
+        checkResult();
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Sudoku'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.replay_outlined),
+            onPressed: () {
+              Timer(const Duration(milliseconds: 200), () => restartGame());
+            },
+          ),
+          IconButton(
+            icon: Icon(Icons.add_rounded),
+            onPressed: () {
+              Timer(const Duration(milliseconds: 200), () => newGame('easy'));
+            },
+          ),
+        ],
+      ),
+      body: Builder(builder: (builder) {
+        return Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: createRows(),
+              ),
+            ),
+            Text(x)
+          ],
+        );
+      }),
     );
   }
 }
